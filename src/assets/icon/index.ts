8458d959946dc5eb5475bb13f475f2752e8cefb6
Doyun Lee <doyn511@ajou.ac.kr>
2023-11-22 03:06:19 +0900

    feat : index.ts에서 icon import

--- src/assets/icon/index.ts
+++ src/assets/icon/index.ts
@@ -1,3 +1,7 @@
 import IcCgvLogo from './ic_cgv_logo.svg?react';
+import ICFavoriteOff from "./ic_favorite_off_24px.svg?react";
+import ICFavoriteOn from "./ic_favorite_on_24px.svg?react";
+import ICHamburger from "./ic_hamburger_24px.svg?react";
 
-export { IcCgvLogo };
+export { ICFavoriteOff, ICFavoriteOn, ICHamburger, IcCgvLogo };
+
