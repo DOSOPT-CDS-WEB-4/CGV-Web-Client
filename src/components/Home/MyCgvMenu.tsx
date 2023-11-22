20a8a2b8938631dfd0b2c35f4d68e6a7c86031ad
namdaeun <nde40345@gmail.com>
2023-11-23 02:55:00 +0900

    init : 기본 구조 세팅

---
+++ src/components/Home/MyCgvMenu.tsx
@@ -0,0 +1,69 @@
+import styled from 'styled-components';
+
+import img_menu1 from '../../assets/image/img_menu1.png';
+import img_menu2 from '../../assets/image/img_menu2.png';
+import img_menu3 from '../../assets/image/img_menu3.png';
+import img_menu4 from '../../assets/image/img_menu4.png';
+
+const MyCgvMenu = () => {
+  return (
+    <>
+      <St.MenuHeader />
+      <St.MenuWrapper>
+        <St.LI>
+          <img src={img_menu1} className='left' alt='my cgv' />
+          <p className='left-txt'>MY CGV</p>
+        </St.LI>
+        <St.LI>
+          <img src={img_menu2} alt='포토플레이' />
+          <p>포토플레이</p>
+        </St.LI>
+        <St.LI>
+          <img src={img_menu3} alt='할인정보' />
+          <p>할인정보</p>
+        </St.LI>
+        <St.LI>
+          <img src={img_menu4} alt='CGV 스토어' />
+          <p>CGV스토어</p>
+        </St.LI>
+      </St.MenuWrapper>
+      <St.MenuFooter />
+    </>
+  );
+};
+
+export default MyCgvMenu;
+
+const St = {
+  MenuHeader: styled.div`
+
+    `,
+
+  MenuWrapper: styled.div`
+
+    `,
+
+  LI: styled.li`
+
+        & > img {
+
+        }
+
+        & > img.left {
+
+        }
+
+        & > p {
+
+        }
+
+        & > p.left-txt {
+
+        }
+    `,
+
+  MenuFooter: styled.div`
+
+    `,
+
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,9 @@
-import { IcCgvLogo } from '../assets/icon';
-import img_movie_1 from '../assets/image/img_movie1.png';
+import MyCgvMenu from '../components/Home/MyCgvMenu';
 
 const Home = () => {
   return (
     <div>
-      홈 화면 페이지입니다.
-      <IcCgvLogo />
-      <img src={img_movie_1} alt="영화-이미지-1" />
+      <MyCgvMenu />
     </div>
   );
 };
