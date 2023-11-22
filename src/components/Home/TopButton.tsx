1fb623435386b606599f009ce5f74751462ea7fb
namdaeun <nde40345@gmail.com>
2023-11-23 03:28:55 +0900

    feat : 버튼 위치 고정

--- src/components/Home/TopButton.tsx
+++ src/components/Home/TopButton.tsx
@@ -1,11 +1,23 @@
+import styled from "styled-components";
+
 import { IcBtnUp } from "../../assets/icon";
 
 const TopButton = () => {
   return (
-    <>
-      <IcBtnUp />
-    </>
+    <St.BtnWrapper>
+      <IcBtnUp className="button" />
+    </St.BtnWrapper>
   );
 };
 
 export default TopButton;
+
+const St = {
+  BtnWrapper: styled.div`
+    & > .button {
+      position: fixed;
+      top: 76.5rem;
+      left: 31.5rem;
+    }
+  `,
+};
