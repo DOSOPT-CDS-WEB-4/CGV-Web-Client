c4bdca891ddf93ec3632ccfa5fce432ad5d194e6
namdaeun <nde40345@gmail.com>
2023-11-23 03:35:57 +0900

    feat : 버튼 onClick 속성 설정 & 핸들러 함수 정의

--- src/components/Home/TopButton.tsx
+++ src/components/Home/TopButton.tsx
@@ -3,9 +3,19 @@ import styled from "styled-components";
 import { IcBtnUp } from "../../assets/icon";
 
 const TopButton = () => {
+
+  const handleToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+
+  };
   return (
     <St.BtnWrapper>
-      <IcBtnUp className="button" />
+      <IcBtnUp
+        className="button"
+        onClick={handleToTop} />
     </St.BtnWrapper>
   );
 };
@@ -16,7 +26,7 @@ const St = {
   BtnWrapper: styled.div`
     & > .button {
       position: fixed;
-      top: 76.5rem;
+      top: 75.5rem;
       left: 31.5rem;
     }
   `,
