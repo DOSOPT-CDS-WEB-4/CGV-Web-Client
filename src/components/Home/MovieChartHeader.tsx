098f10a86af4398adf4ef09475696bf8a53f3584
namdaeun <nde40345@gmail.com>
2023-11-23 01:46:52 +0900

    feat : 서브 메뉴차트 가로 스크롤

--- src/components/Home/MovieChartHeader.tsx
+++ src/components/Home/MovieChartHeader.tsx
@@ -30,7 +30,6 @@ const MovieChartHeader = () => {
         </St.LI>
       </St.SubChartWrapper>
     </>
-
   );
 };
 
@@ -38,67 +37,74 @@ export default MovieChartHeader;
 
 const St = {
   MovieChartWrapper: styled.div`
-        & > p {
-            display: inline;
-            flex-direction: column;
-            justify-content: center;
-            flex-shrink: 0;
-            ${({ theme }) => theme.fonts['title_bold_18']};
-        }
-
-        & > p.movie-chart {
-            margin: 1.9rem 0.4rem 2.1rem 1.5rem;
-            color: ${({ theme }) => theme.colors['red']};
-        }
-
-        & > p.scheduled {
-            margin: 1.9rem 14.4rem 2.1rem 0.4rem;
-            color: ${({ theme }) => theme.colors['gray400']};
-        }
-
-        & > p.view-all {
-            margin: 1.8rem 0.2rem 0.4rem 0rem;
-            flex-shrink: 0;
-            color: ${({ theme }) => theme.colors['gray400']};
-            ${({ theme }) => theme.fonts['body_regular_13']};
-        }
-
-        & > .arrow {
-          flex-shrink: 0;
-          margin: 1.8rem 1.5rem 0rem 0.2rem;
-
-        }
-    `,
-
-  SubChartWrapper: styled.div`
-
-`,
-  LI: styled.li`
-    display: inline-block; 
-    list-style-type: none;
-
     & > p {
-        text-align: center;
-        ${({ theme }) => theme.fonts['body_regular_14']};
+        display: inline;
+        flex-direction: column;
+        justify-content: center;
+        flex-shrink: 0;
+        ${({ theme }) => theme.fonts['title_bold_18']};
     }
 
-    & > p.selected {
+    & > p.movie-chart {
+        margin: 1.9rem 0.4rem 2.1rem 1.5rem;
+        color: ${({ theme }) => theme.colors['red']};
+    }
+
+    & > p.scheduled {
+        margin: 1.9rem 14.4rem 2.1rem 0.4rem;
+        color: ${({ theme }) => theme.colors['gray400']};
+    }
+
+    & > p.view-all {
+        margin: 1.8rem 0.2rem 0.4rem 0rem;
+        flex-shrink: 0;
+        color: ${({ theme }) => theme.colors['gray400']};
+        ${({ theme }) => theme.fonts['body_regular_13']};
+    }
+
+    & > .arrow {
+        flex-shrink: 0;
+        margin: 1.8rem 1.5rem 0rem 0.2rem;
+
+    }
+  `,
+
+  SubChartWrapper: styled.div`
+    max-width: 100%;
+    white-space: nowrap;
+    overflow-x: auto;
+          
+    &::-webkit-scrollbar {
+      display: none;
+    }
+  `,
+
+  LI: styled.li`
+      display: inline-block; 
+      list-style-type: none;
+
+      & > p {
+        text-align: center;
+        ${({ theme }) => theme.fonts['body_regular_14']};
+      }
+
+      & > p.selected {
         color: ${({ theme }) => theme.colors['red']};
         padding: 2.1rem 0rem 0.8rem 0rem;
         margin: 0rem 0rem 0rem 1.5rem;
         border-bottom: 1px solid ${({ theme }) => theme.colors['red']};
-    }
+      }
 
-    & > p.next-to-selected {
+      & > p.next-to-selected {
         color: ${({ theme }) => theme.colors['gray600']};
         padding: 2.1rem 2.2rem 0.8rem 2.2rem;
         border-bottom: 1px solid ${({ theme }) => theme.colors['gray300']};
-    }
+      }
 
-    & > p.not-selected {
+      & > p.not-selected {
         color: ${({ theme }) => theme.colors['gray600']};
         padding: 2.1rem 2.2rem 0.8rem 0rem;
         border-bottom: 1px solid ${({ theme }) => theme.colors['gray300']};
-    }
+      }
     `,
 };
