6e2ade05c3b431aa82ab859fbbb4e41ede8e1938
Doyun Lee <doyn511@ajou.ac.kr>
2023-11-22 23:48:24 +0900

    fix : svg명 변경, 중복 svg 처리

--- src/assets/icon/index.ts
+++ src/assets/icon/index.ts
@@ -1,7 +1,6 @@
 import IcCgvLogo from './ic_cgv_logo.svg?react';
-import ICFavoriteOff from "./ic_favorite_off_24px.svg?react";
-import ICFavoriteOn from "./ic_favorite_on_24px.svg?react";
-import ICHamburger from "./ic_hamburger_24px.svg?react";
+import ICFavoriteOff from './ic_favorite_off.svg?react';
+import ICFavoriteOn from './ic_favorite_on.svg?react';
+import ICHamburger from './ic_hamburger_white.svg?react';
 
 export { ICFavoriteOff, ICFavoriteOn, ICHamburger, IcCgvLogo };
-
