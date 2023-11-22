f392073ad3666293aaaa755842c41868e46dc1d9
namdaeun <nde40345@gmail.com>
2023-11-23 00:07:11 +0900

    docs : icons file name 변경

--- src/assets/icon/index.ts
+++ src/assets/icon/index.ts
@@ -2,15 +2,15 @@ import IcCgvLogo from './ic_cgv_logo.svg?react';
 
 export { IcCgvLogo };
 
-import IcHamburger from './ic_hamburger_24px.svg?react';
+import IcHamburger from './ic_hamburger_black.svg?react';
 
 export { IcHamburger };
 
-import IcSearch from './ic_search_24px.svg?react';
+import IcSearch from './ic_search.svg?react';
 
 export { IcSearch };
 
-import IcTicket from './ic_ticket_30px.svg?react';
+import IcTicket from './ic_ticket.svg?react';
 
 export { IcTicket };
 
