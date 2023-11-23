046e53a19736acba90243bf6256ca4b74bbc3a01
Doyun Lee <doyn511@ajou.ac.kr>
2023-11-24 03:41:09 +0900

    style : stylelintric 자동정렬 적용

--- src/components/SelectTime/SelectTimeHeader.tsx
+++ src/components/SelectTime/SelectTimeHeader.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+
 import { ICHamburger } from '../../assets/icon';
 const SelectTimeHeader = () => {
   return (
@@ -16,32 +17,32 @@ export default SelectTimeHeader;
 const St = {
   HeaderWrapper: styled.header`
     display: inline-flex;
+    gap: 11.9rem;
     align-items: center;
 
     padding: 1.2rem 16rem 1.2rem 1.6rem;
-    gap: 11.9rem;
 
     background-color: transparent;
   `,
 
   MenuIcon: styled.button`
+    cursor: pointer;
+
     display: flex;
-    justify-content: center;
     align-items: center;
+    justify-content: center;
 
     width: 2.4rem;
     height: 2.4rem;
     padding: 0.4rem 0.2rem;
-
-    cursor: pointer;
   `,
 
   PageTitle: styled.h1`
     width: 5.6rem;
-
-    color: ${({ theme }) => theme.colors.white};
+    text-align: center;
 
     ${({ theme }) => theme.fonts.body_medium_15};
-    text-align: center;
+
+    color: ${({ theme }) => theme.colors.white};
   `,
 };
