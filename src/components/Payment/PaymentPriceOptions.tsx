df2cba2477b6f49e9529d9c729cf0aa83fa31f38
Yeonseo-Jo <jys073190@gmail.com>
2023-11-24 18:34:35 +0900

    feat : 토스 간편 결제하기 배너 부분 구현

--- src/components/Payment/PaymentPriceOptions.tsx
+++ src/components/Payment/PaymentPriceOptions.tsx
@@ -1,5 +1,35 @@
+import styled from 'styled-components';
+
+import img_toss_banner from '../../assets/image/img_toss_banner.png';
+
 const PaymentPriceOptions = () => {
-  return <div></div>;
+  return (
+    <St.PriceOptionsWrapper>
+      <St.TossBannerContainer>
+        <St.TossBannerImg src={img_toss_banner} alt="토스-간편결제-배너" />
+      </St.TossBannerContainer>
+    </St.PriceOptionsWrapper>
+  );
 };
 
 export default PaymentPriceOptions;
+
+const St = {
+  PriceOptionsWrapper: styled.section`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+  `,
+
+  TossBannerContainer: styled.article`
+    display: flex;
+    justify-content: center;
+    padding: 0.8rem 1.5rem;
+    background-color: ${({ theme }) => theme.colors.gray100};
+  `,
+
+  TossBannerImg: styled.img`
+    width: 34.5rem;
+    height: 3.2rem;
+  `,
+};
