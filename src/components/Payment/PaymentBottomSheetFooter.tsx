68c67e522a43a528a9e81c64d909df908407ffce
Yeonseo-Jo <jys073190@gmail.com>
2023-11-25 05:31:52 +0900

    feat : payment 페이지 footer 컴포넌트 추가

---
+++ src/components/Payment/PaymentBottomSheetFooter.tsx
@@ -0,0 +1,5 @@
+const PaymentBottomSheetFooter = () => {
+  return <></>;
+};
+
+export default PaymentBottomSheetFooter;
--- src/pages/Payment.tsx
+++ src/pages/Payment.tsx
@@ -1,5 +1,12 @@
+import PaymentBottomSheetFooter from '../components/Payment/PaymentBottomSheetFooter';
+
 const Payment = () => {
-  return <div>결제하기 페이지입니다.</div>;
+  return (
+    <div>
+      결제하기 페이지입니다.
+      <PaymentBottomSheetFooter />
+    </div>
+  );
 };
 
 export default Payment;
