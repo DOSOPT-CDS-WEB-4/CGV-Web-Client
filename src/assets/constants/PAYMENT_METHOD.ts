0b5a16ca214bb00c280c29de0f414f77ed1e6a68
Yeonseo-Jo <jys073190@gmail.com>
2023-11-25 04:34:59 +0900

    feat : 결제 수단 상수 추가

---
+++ src/assets/constants/PAYMENT_METHOD.ts
@@ -0,0 +1,11 @@
+import ic_card from '../icon/ic_card.svg';
+import img_toss from '../image/img_toss.png';
+
+export const PAYMENT_METHOD = [
+  { id: 1, icon: ic_card, description: '스마트결제' },
+  { id: 2, icon: ic_card, description: '스마트결제' },
+  { id: 3, icon: ic_card, description: '스마트결제' },
+  { id: 4, icon: ic_card, description: '스마트결제' },
+  { id: 5, icon: ic_card, description: '스마트결제' },
+  { id: 6, icon: img_toss, description: '' },
+];
