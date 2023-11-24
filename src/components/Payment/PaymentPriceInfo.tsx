c782839bd43c7f2b02a81d07065f3f2fe466b966
Yeonseo-Jo <jys073190@gmail.com>
2023-11-25 03:32:52 +0900

    feat : 최상단 텍스트 영역 구현

--- src/components/Payment/PaymentPriceInfo.tsx
+++ src/components/Payment/PaymentPriceInfo.tsx
@@ -1,5 +1,56 @@
+import styled from 'styled-components';
+
+import { IcCheck } from '../../assets/icon';
+
 const PaymentPriceInfo = () => {
-  return <div></div>;
+  return (
+    <St.PaymentPriceInfoWrapper>
+      <St.PriceInfoTextArea>
+        <St.PriceInfoTitle>결제정보</St.PriceInfoTitle>
+        <St.PriceInfoSaveLastBox>
+          <IcCheck />
+          <St.SaveLastText>마지막 결제수단 저장</St.SaveLastText>
+        </St.PriceInfoSaveLastBox>
+      </St.PriceInfoTextArea>
+    </St.PaymentPriceInfoWrapper>
+  );
 };
 
 export default PaymentPriceInfo;
+
+const St = {
+  PaymentPriceInfoWrapper: styled.section`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    width: 37.5rem;
+    height: 24.4rem;
+    padding: 0 1.5rem;
+  `,
+
+  PriceInfoTextArea: styled.div`
+    display: flex;
+    justify-content: space-between;
+
+    width: 100%;
+    padding: 1.8rem 0 1.6rem 0;
+
+    text-align: center;
+  `,
+
+  PriceInfoTitle: styled.h3`
+    color: ${({ theme }) => theme.colors.gray900};
+    ${({ theme }) => theme.fonts.body_regular_16};
+  `,
+
+  PriceInfoSaveLastBox: styled.div`
+    display: flex;
+    align-items: center;
+  `,
+
+  SaveLastText: styled.p`
+    color: ${({ theme }) => theme.colors.coral};
+    ${({ theme }) => theme.fonts.body_regular_14};
+  `,
+};
