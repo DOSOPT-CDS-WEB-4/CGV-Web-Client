9e6eef754026aa8531897357ae9ec4fbe19da079
Yeonseo-Jo <jys073190@gmail.com>
2023-11-24 18:24:48 +0900

    feature : 가격 옵션 부분 component 생성 및 페이지에 추가

---
+++ src/components/Payment/PaymentPriceOptions.tsx
@@ -0,0 +1,5 @@
+const PaymentPriceOptions = () => {
+  return <div></div>;
+};
+
+export default PaymentPriceOptions;
--- src/pages/Payment.tsx
+++ src/pages/Payment.tsx
@@ -1,5 +1,12 @@
+import PaymentPriceOptions from '../components/Payment/PaymentPriceOptions';
+
 const Payment = () => {
-  return <div>결제하기 페이지입니다.</div>;
+  return (
+    <div>
+      결제하기 페이지입니다.
+      <PaymentPriceOptions />
+    </div>
+  );
 };
 
 export default Payment;
