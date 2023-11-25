9c3239815b03ee8b211be7f0c4400b9946e4b364
Yeonseo-Jo <jys073190@gmail.com>
2023-11-26 03:09:06 +0900

    feat : bottomsheet 기본 layout 구현

--- src/components/SelectTime/SelectPerson/SelectPersonBottomSheet.tsx
+++ src/components/SelectTime/SelectPerson/SelectPersonBottomSheet.tsx
@@ -1,5 +1,63 @@
+import styled from 'styled-components';
+
+import SelectPersonBottomSheetPortal from './SelectPersonBottomSheetPortal';
+
 const SelectPersonBottomSheet = () => {
-  return <div></div>;
+  return (
+    <SelectPersonBottomSheetPortal>
+      <>
+        <St.BottomSheetBg></St.BottomSheetBg>
+        <St.BottomSheetConatiner>
+          바텀시트 부분
+          <St.BottomSheetSelectPersonBtn type="button">인원선택</St.BottomSheetSelectPersonBtn>
+        </St.BottomSheetConatiner>
+      </>
+    </SelectPersonBottomSheetPortal>
+  );
 };
 
 export default SelectPersonBottomSheet;
+
+const St = {
+  BottomSheetBg: styled.div`
+    position: fixed;
+    top: 0;
+
+    width: 100%;
+    max-width: 43rem;
+    height: 100vh;
+
+    opacity: 0.5;
+    background-color: ${({ theme }) => theme.colors.black};
+  `,
+
+  BottomSheetConatiner: styled.section`
+    position: fixed;
+    z-index: 10;
+    bottom: 0;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    width: 100%;
+    max-width: 43rem;
+    height: calc(100vh - 23.2rem);
+
+    background-color: white;
+  `,
+
+  BottomSheetSelectPersonBtn: styled.button`
+    display: flex;
+    justify-content: center;
+
+    width: 100%;
+    padding: 1.8rem 0 5.3rem 0;
+
+    color: ${({ theme }) => theme.colors.white};
+
+    background: ${({ theme }) => theme.colors.gradient};
+
+    ${({ theme }) => theme.fonts.title_medium_18};
+  `,
+};
---
+++ src/pages/SelectPersonTest.tsx
@@ -0,0 +1,12 @@
+import SelectPersonBottomSheet from '../components/SelectTime/SelectPerson/SelectPersonBottomSheet';
+
+const SelectPersonTest = () => {
+  return (
+    <>
+      <button type="button">bottomsheet 나오기!</button>
+      <SelectPersonBottomSheet />
+    </>
+  );
+};
+
+export default SelectPersonTest;
