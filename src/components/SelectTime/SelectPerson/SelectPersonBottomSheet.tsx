250fc5939c0f68ab86e50c254aae72ade4cc0a48
Yeonseo-Jo <jys073190@gmail.com>
2023-11-26 01:40:19 +0900

    feat : bottomsheet 관련 컴포넌트 생성

---
+++ src/components/SelectTime/SelectPerson/SelectPersonBottomSheet.tsx
@@ -0,0 +1,5 @@
+const SelectPersonBottomSheet = () => {
+  return <div></div>;
+};
+
+export default SelectPersonBottomSheet;
---
+++ src/components/SelectTime/SelectPerson/SelectPersonBottomSheetPortal.tsx
@@ -0,0 +1,5 @@
+const SelectPersonBottomSheetPortal = () => {
+  return <div></div>;
+};
+
+export default SelectPersonBottomSheetPortal;
