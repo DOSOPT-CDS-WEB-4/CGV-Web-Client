75fa6f23557c42e313d332016b136e4c5ccfd83f
Yeonseo-Jo <jys073190@gmail.com>
2023-11-26 02:24:32 +0900

    feat : bottom-sheet용 portal create

--- src/components/SelectTime/SelectPerson/SelectPersonBottomSheetPortal.tsx
+++ src/components/SelectTime/SelectPerson/SelectPersonBottomSheetPortal.tsx
@@ -1,5 +1,9 @@
-const SelectPersonBottomSheetPortal = () => {
-  return <div></div>;
+import ReactDOM from 'react-dom';
+
+const SelectPersonBottomSheetPortal = ({ children }: { children: React.ReactElement }) => {
+  const targetEl = document.getElementById('bottom-sheet') as HTMLElement;
+
+  return ReactDOM.createPortal(children, targetEl);
 };
 
 export default SelectPersonBottomSheetPortal;
