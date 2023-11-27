5a7f422a2d12a857ecb5b9182cfc130fcc2dd71b
namdaeun <nde40345@gmail.com>
2023-11-27 12:36:35 +0900

    docs : index.ts export 한 번에 해주기

--- src/assets/icon/index.ts
+++ src/assets/icon/index.ts
@@ -1,16 +1,7 @@
 import IcCgvLogo from './ic_cgv_logo.svg?react';
-
-export { IcCgvLogo };
-
 import IcHamburger from './ic_hamburger_black.svg?react';
-
-export { IcHamburger };
-
 import IcSearch from './ic_search.svg?react';
-
-export { IcSearch };
-
 import IcTicket from './ic_ticket.svg?react';
 
-export { IcTicket };
+export { IcCgvLogo, IcHamburger, IcSearch, IcTicket };
 
