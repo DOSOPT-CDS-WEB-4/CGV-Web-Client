3fca08463a9f27a2b8caef9403203d233deca0ca
namdaeun <nde40345@gmail.com>
2023-11-28 04:28:05 +0900

    feat : 무비카드가 선택되어야만 플로팅 버튼 활성화

--- src/components/Home/BookingFloatingButton.tsx
+++ src/components/Home/BookingFloatingButton.tsx
@@ -1,8 +1,23 @@
+import React from 'react';
+import { useNavigate } from 'react-router';
 import styled from 'styled-components';
 
-const BookingFloatingButton = () => {
+interface BookingFloatingButtonProps {
+  selectedCard: boolean;
+}
+
+const BookingFloatingButton: React.FC<BookingFloatingButtonProps> = ({ selectedCard }) => {
+  const navigate = useNavigate();
+
+  const handleBtn = () => {
+    navigate('/select-time');
+  };
+
   return (
-    <St.BtnWrapper>
+    <St.BtnWrapper
+      onClick={handleBtn}
+      disabled={!selectedCard}
+    >
       <St.TextWrapper>
         <St.SmallTxt>빠르고 쉽게</St.SmallTxt>
         <St.BigTxt>지금예매</St.BigTxt>
@@ -15,37 +30,37 @@ export default BookingFloatingButton;
 
 const St = {
   BtnWrapper: styled.button`
-      position: fixed;
-      top: 63.4rem;
-      left: 25.2rem;
+    position: fixed;
+    top: 63.4rem;
+    left: 25.2rem;
 
-      flex-shrink: 0;
+    flex-shrink: 0;
 
-      width: 15.8rem;
-      height: 5.5rem;
+    width: 15.8rem;
+    height: 5.5rem;
 
-      background: ${({ theme }) => theme.colors['gradient']};
-      border-radius: 5rem;
-      box-shadow: 5px 6px 6px 0px rgba(0, 0, 0, 0.40);
-    `,
+    background: ${({ theme }) => theme.colors['gradient']};
+    border-radius: 5rem;
+    box-shadow: 5px 6px 6px 0px rgba(0, 0, 0, 0.40);
+  `,
 
   TextWrapper: styled.div`
-      flex-direction: column;
-      flex-shrink: 0;
+    flex-direction: column;
+    flex-shrink: 0;
 
-      width: 6rem;
-      margin: 1.3rem 7.6rem 1.2rem 2.2rem;
+    width: 6rem;
+    margin: 1.3rem 7.6rem 1.2rem 2.2rem;
 
-      text-align: left;
-    `,
+    text-align: left;
+  `,
 
   SmallTxt: styled.div`
-      color: ${({ theme }) => theme.colors['white']};
-      ${({ theme }) => theme.fonts['body_regular_10']};
-    `,
+    color: ${({ theme }) => theme.colors['white']};
+    ${({ theme }) => theme.fonts['body_regular_10']};
+  `,
 
   BigTxt: styled.div`
-      color: ${({ theme }) => theme.colors['white']};
-      ${({ theme }) => theme.fonts['body_semibold_16']};
-    `,
+    color: ${({ theme }) => theme.colors['white']};
+    ${({ theme }) => theme.fonts['body_semibold_16']};
+  `,
 };
