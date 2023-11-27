e352cd95d49e28957fc17bdb19c63c4fb19bdc9f
Doyun Lee <108219121+doyn511@users.noreply.github.com>
2023-11-28 03:10:23 +0900

    Revert "[SelectTime] 영화 정보 section UI 구현"

--- src/assets/icon/index.ts
+++ src/assets/icon/index.ts
@@ -1,6 +1,3 @@
 import IcCgvLogo from './ic_cgv_logo.svg?react';
-import ICFavoriteOff from './ic_favorite_off.svg?react';
-import ICFavoriteOn from './ic_favorite_on.svg?react';
-import ICHamburger from './ic_hamburger_white.svg?react';
 
-export { ICFavoriteOff, ICFavoriteOn, ICHamburger, IcCgvLogo };
+export { IcCgvLogo };
--- src/pages/SelectTime.tsx
+++ src/pages/SelectTime.tsx
@@ -1,11 +1,5 @@
-import MovieInfo from '../components/SelectTime/MovieInfo';
-
 const SelectTime = () => {
-  return (
-    <>
-      <MovieInfo />
-    </>
-  );
+  return <div>관람 시간 선택 페이지입니다.</div>;
 };
 
 export default SelectTime;
