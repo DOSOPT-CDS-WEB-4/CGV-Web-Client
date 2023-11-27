d7202d850598e1bb7698b5ee1c74541abc9374c6
Doyun Lee <doyn511@ajou.ac.kr>
2023-11-28 01:17:00 +0900

    fix : SelectTime에서 Footer.jsx 삭제

--- src/pages/SelectTime.tsx
+++ src/pages/SelectTime.tsx
@@ -1,11 +1,9 @@
-import SelectTimeFooter from '../components/SelectTime/Footer';
 import MovieInfo from '../components/SelectTime/MovieInfo';
 
 const SelectTime = () => {
   return (
     <>
       <MovieInfo />
-      <SelectTimeFooter />
     </>
   );
 };
