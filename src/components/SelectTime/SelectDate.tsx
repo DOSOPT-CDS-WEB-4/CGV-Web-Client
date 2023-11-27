d3d499ce8e03d549d9e95f43ee36da7adaba3a4d
Doyun Lee <doyn511@ajou.ac.kr>
2023-11-27 20:16:57 +0900

    feat : SelectTime.jsx 생성

---
+++ src/components/SelectTime/SelectDate.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const SelectDate = () => {
+  return <section></section>;
+};
+
+export default SelectDate;
+
+const St = {
+  SelectDateWapper: styled.section``,
+};
