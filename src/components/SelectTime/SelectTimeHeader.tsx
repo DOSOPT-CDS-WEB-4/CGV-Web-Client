57b6a0748d513ed14cbc1cfbaf831851d091cce4
Doyun Lee <doyn511@ajou.ac.kr>
2023-11-28 02:40:32 +0900

    fix : Button 태그 삭제

--- src/components/SelectTime/SelectTimeHeader.tsx
+++ src/components/SelectTime/SelectTimeHeader.tsx
@@ -4,9 +4,7 @@ import { ICHamburger } from '../../assets/icon';
 const SelectTimeHeader = () => {
   return (
     <St.HeaderWrapper>
-      <St.MenuIcon type="button">
-        <ICHamburger />
-      </St.MenuIcon>
+      <ICHamburger />
       <St.PageTitle>빠른 예매</St.PageTitle>
     </St.HeaderWrapper>
   );
@@ -23,18 +21,17 @@ const St = {
     padding: 1.2rem 16rem 1.2rem 1.6rem;
 
     background-color: transparent;
+
+    & > svg {
+      cursor: pointer;
+    }
   `,
 
   MenuIcon: styled.button`
     cursor: pointer;
-
     display: flex;
     align-items: center;
     justify-content: center;
-
-    width: 2.4rem;
-    height: 2.4rem;
-    padding: 0.4rem 0.2rem;
   `,
 
   PageTitle: styled.h1`
