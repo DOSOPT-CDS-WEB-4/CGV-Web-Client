2cce26ff6d358b3ee2658645d17dfe1e9e08c4bb
namdaeun <nde40345@gmail.com>
2023-11-27 16:53:55 +0900

    docs : header에 padding주기

--- src/components/Home/HomeHeader.tsx
+++ src/components/Home/HomeHeader.tsx
@@ -34,23 +34,24 @@ export default HomeHeader;
 const St = {
   HeaderWrapper: styled.div`
     width: 100%;
+    padding : 1.3rem 1.5rem 1rem 1.5rem;
 
     & > .hamburger {
-      margin: 1.7rem 0rem 1.5rem 1.7rem;
+      margin-right: 11.7rem;
     }
 
     & > .logo {
       width: 6rem;
       height: 2.5rem;
-      margin: 1.3rem 8.7rem 1.0rem 11.9rem;
+      margin-right: 8.7rem;
     }
 
     & > .search {
-      margin: 1.5rem 0.3rem 1.1rem 0rem;
+      margin-right: 0.3rem;
     }
 
     & > .ticket {
-      margin: 1.5rem 1.5rem 1.1rem 0rem;
+      margin-left: auto;
     }
 `,
 };
