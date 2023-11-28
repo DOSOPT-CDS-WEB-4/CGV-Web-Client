c05c71f0653ce3e7787220815785b46abade2e62
Doyun Lee <doyn511@ajou.ac.kr>
2023-11-28 16:05:44 +0900

    feat : TimeTable 컴포넌트 분리 및 예매종료 event

--- src/components/SelectTime/SelectMovie.jsx
+++ src/components/SelectTime/SelectMovie.jsx
@@ -2,41 +2,60 @@ import { useState } from 'react';
 import styled from 'styled-components';
 
 import { ICFavoriteOff, ICFavoriteOn } from '../../assets/icon';
+import TimeTable from './TimeTable';
 
 const DATA_LIST = {
   region_names: '홍대',
   region_far: '1.3km',
   screen_types: ['전체', '일반관', 'IMAX관', '컴포트관', 'GOLD CLASS관', '4DX관'],
-  movie_screen_schedules: {
-    screenType: '컴포트관',
-    place: '1관 8층',
-    moivetype: '자막',
-    screenactive: '2D',
-    startTime: '12:15',
-    endTime: '14:00',
-    totalSeats: 200,
-    emptySeats: 200,
-    reservationAvailability: true,
-  },
+  movie_screen_schedules: [
+    {
+      startTime: '12:15',
+      endTime: '14:00',
+      emptySeats: 200,
+      reservationAvailability: false,
+    },
+    {
+      startTime: '12:15',
+      endTime: '14:00',
+
+      emptySeats: 200,
+      reservationAvailability: true,
+    },
+  ],
+
+  screenType: '컴포트관',
+  place: '1관 8층',
+  moivetype: '자막',
+  screenactive: '2D',
+  totalSeats: 200,
 };
 
 const SelectMovieTime = () => {
   const [clickLike, setClickLike] = useState(false);
-  const { region_names, region_far, screen_types } = DATA_LIST;
+  const [typeClick, setTypeClick] = useState([]);
+
   const {
+    region_names,
+    region_far,
+    screen_types,
     screenType,
     place,
     moivetype,
     screenactive,
     totalSeats,
-    startTime,
-    endTime,
-    emptySeats,
-    reservationAvailability,
-  } = DATA_LIST.movie_screen_schedules;
+  } = DATA_LIST;
 
   const handleOnClickLike = () => {
     setClickLike(!clickLike);
+  };
+
+  const handleTypeClick = type => {
+    if (typeClick.includes(type)) {
+      setTypeClick(typeClick.filter(elm => elm !== type));
+    } else {
+      setTypeClick([...typeClick, type]);
+    }
   };
 
   return (
@@ -57,7 +76,15 @@ const SelectMovieTime = () => {
       </St.SelectedRegion>
       <St.ScreenTypeWrapper>
         {screen_types.map(type => (
-          <St.EachType key={type}>{type}</St.EachType>
+          <St.EachType
+            key={type}
+            onClick={() => {
+              handleTypeClick(type);
+            }}
+            $isClicked={typeClick.includes(type)}
+          >
+            {type}
+          </St.EachType>
         ))}
       </St.ScreenTypeWrapper>
       <St.SelectTime>
@@ -71,15 +98,19 @@ const SelectMovieTime = () => {
           </St.InfoRight>
         </St.Info>
         <St.TimeTableList>
-          <St.TimeTable>
-            <St.TimeWrapper>
-              <St.StartTime>{startTime}</St.StartTime>
-              <St.EndTime>~{endTime}</St.EndTime>
-            </St.TimeWrapper>
-            <St.EmptySeatsWrapper>
-              <St.EmptySeats>잔여 {emptySeats}석</St.EmptySeats>
-            </St.EmptySeatsWrapper>
-          </St.TimeTable>
+          {DATA_LIST.movie_screen_schedules.map((movieSchedules, index) => {
+            const { reservationAvailability, startTime, endTime, emptySeats } = movieSchedules;
+
+            return (
+              <TimeTable
+                key={index}
+                startTime={startTime}
+                endTime={endTime}
+                emptySeats={emptySeats}
+                reservationAvailability={reservationAvailability}
+              />
+            );
+          })}
         </St.TimeTableList>
       </St.SelectTime>
     </St.SelectMovieWrapper>
@@ -144,8 +175,10 @@ const St = {
     vertical-align: center;
 
     border-radius: 1.6rem;
-    border: 1px solid ${({ theme }) => theme.colors.gray600};
-    color: ${({ theme }) => theme.colors.gray600};
+    border: 1px solid
+      ${({ $isClicked, theme }) => ($isClicked ? theme.colors.gradient : theme.colors.gray600)};
+    color: ${({ $isClicked, theme }) =>
+      $isClicked ? theme.colors.gradient : theme.colors.gray600};
   `,
 
   SelectTime: styled.section`
@@ -181,51 +214,6 @@ const St = {
   Location: styled.span`
     color: ${({ theme }) => theme.colors.gray700};
     ${({ theme }) => theme.fonts.body_medium_12};
-  `,
-
-  TimeTable: styled.button`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-
-    width: 10.7rem;
-    height: 6.8rem;
-    flex-shrink: 0;
-
-    overflow: hidden;
-    border-radius: 0.6rem;
-    border: 1px solid ${({ theme }) => theme.colors.red};
-  `,
-
-  TimeWrapper: styled.div`
-    display: flex;
-    align-items: center;
-    height: 4.2rem;
-  `,
-
-  StartTime: styled.span`
-    color: ${({ theme }) => theme.colors.gray900};
-    ${({ theme }) => theme.fonts.body_semibold_16};
-  `,
-
-  EndTime: styled.span`
-    color: ${({ theme }) => theme.colors.gray600};
-    ${({ theme }) => theme.fonts.body_semibold_12};
-  `,
-
-  EmptySeatsWrapper: styled.div`
-    width: 10.7rem;
-    height: 2.6rem;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-    background-color: ${({ theme }) => theme.colors.gray100};
-  `,
-
-  EmptySeats: styled.span`
-    color: ${({ theme }) => theme.colors.red};
-    ${({ theme }) => theme.fonts.body_semibold_12};
   `,
 
   TimeTableList: styled.div`
