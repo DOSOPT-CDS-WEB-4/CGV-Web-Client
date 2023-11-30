ce9429c5bb3a69d2a4804d79d6aeef7147ad0bfc
Doyun Lee <doyn511@ajou.ac.kr>
2023-12-01 06:01:43 +0900

    chore : getSelectTimeAPI 삭제

--- src/pages/SelectTime.tsx
+++ src/pages/SelectTime.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { useRecoilState, useSetRecoilState } from 'recoil';
+import { useSetRecoilState } from 'recoil';
 import styled from 'styled-components';
 
 import Footer from '../components/SelectTime/Footer';
@@ -7,7 +7,7 @@ import MovieInfo from '../components/SelectTime/MovieInfo';
 import SelectDate from '../components/SelectTime/SelectDate';
 import SelectMovieTime from '../components/SelectTime/SelectMovie';
 import SelectRegion from '../components/SelectTime/SelectRegion';
-import { api } from '../libs/axios';
+import { getSelectTimeAPI } from '../libs/getSelectTimeApi';
 import {
   currentRegion,
   distance,
@@ -16,8 +16,6 @@ import {
   screenTypes,
   selectTimeMovieInfo,
 } from '../recoil/atom';
-import { apiResponse } from '../types/axios';
-import { SelectTimeResponse } from '../types/SelectTimeResponse';
 
 const SelectTime = () => {
   const setRegionNames = useSetRecoilState(regionNames);
@@ -25,51 +23,19 @@ const SelectTime = () => {
   const setCurRegion = useSetRecoilState(currentRegion);
   const setHowFar = useSetRecoilState(distance);
   const setMovieInfo = useSetRecoilState(selectTimeMovieInfo);
-  const [MovieSchedule, setMovieSchedule] = useRecoilState(movieSchedules);
-
-  const getSelectTimeAPI = async (regionName: string, screenTypes: string[]) => {
-    let newURL: string = `region=${regionName}`;
-    if (regionName !== '' && screenTypes.length !== 0) {
-      screenTypes.forEach(type => {
-        newURL += `&type=${type}`;
-      });
-    }
-    try {
-      const response = await api.get<apiResponse<SelectTimeResponse>>(`/reservation/1?${newURL}`);
-      const { data } = response.data;
-
-      const {
-        current_region,
-        distance,
-        movie_info,
-        movie_screen_schedules,
-        region_names,
-        screen_types,
-      } = data;
-
-      console.log(
-        current_region,
-        distance,
-        movie_info,
-        movie_screen_schedules,
-        region_names,
-        screen_types,
-      );
-
-      setRegionNames(region_names);
-      setScreenTypes(screen_types);
-      setCurRegion(current_region);
-      setHowFar(distance);
-      setMovieSchedule(movie_screen_schedules);
-      setMovieInfo(movie_info);
-      console.log(MovieSchedule);
-    } catch (err) {
-      console.log(err);
-    }
-  };
+  const setMovieSchedule = useSetRecoilState(movieSchedules);
 
   useEffect(() => {
-    getSelectTimeAPI('HONGDAE', []);
+    getSelectTimeAPI(
+      'MOKDONG',
+      [],
+      setRegionNames,
+      setScreenTypes,
+      setCurRegion,
+      setHowFar,
+      setMovieInfo,
+      setMovieSchedule,
+    );
   }, []);
 
   return (
