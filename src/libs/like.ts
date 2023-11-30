2c24061986d3f0099af24dedee02635ce8c24a55
namdaeun <nde40345@gmail.com>
2023-12-01 00:49:47 +0900

    docs : 필요 없는 콘솔 지우기

--- src/libs/like.ts
+++ src/libs/like.ts
@@ -4,7 +4,6 @@ import { api } from "./axios";
 export const patchLikeData = async (movie_id : number) => {
     try{
         const { data } = await api.patch<apiResponse<number>>(`/movie/like/${movie_id}`);
-        console.log(data.message);
         return { message: data.message, isError: false };
     } catch (err) {
         return { isError: true };
