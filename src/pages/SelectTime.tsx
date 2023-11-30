f3ddb0e5eb95851d3803ca9158ba9180f4653fac
Doyun Lee <doyn511@ajou.ac.kr>
2023-12-01 02:47:39 +0900

    feat : api 연결 성공

--- src/pages/SelectTime.tsx
+++ src/pages/SelectTime.tsx
@@ -5,8 +5,46 @@ import MovieInfo from '../components/SelectTime/MovieInfo';
 import SelectDate from '../components/SelectTime/SelectDate';
 import SelectMovieTime from '../components/SelectTime/SelectMovie';
 import SelectRegion from '../components/SelectTime/SelectRegion';
+import { api } from '../libs/axios';
+import { apiResponse } from '../types/axios';
+import { SelectTimeResponse } from '../types/SelectTimeResponse';
 
 const SelectTime = () => {
+  const getSelectTimeAPI = async (regionName: string, screenTypes: string[]) => {
+    let newURL: string = `region=${regionName}`;
+    if (regionName !== '' && screenTypes.length !== 0) {
+      screenTypes.forEach(type => {
+        newURL += `&type=${type}`;
+      });
+    }
+    try {
+      const response = await api.get<apiResponse<SelectTimeResponse>>(`/reservation/1?${newURL}`);
+      const { data } = response.data;
+
+      const {
+        current_region,
+        distance,
+        movie_info,
+        movie_screen_schedules,
+        region_names,
+        screen_types,
+      } = data;
+
+      console.log(
+        current_region,
+        distance,
+        movie_info,
+        movie_screen_schedules,
+        region_names,
+        screen_types,
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  getSelectTimeAPI('HONGDAE', []);
+
   return (
     <>
       <MovieInfo />
